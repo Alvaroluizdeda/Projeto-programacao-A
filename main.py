76f40cb3a02a54831dea59e971db392b751b45ab
carlos <carlosgustavomoreira26@gmail.com>
2026-07-10 04:23:33 -0300

    adicionei cores para as bordas e organizei o MAIN

--- main.py
+++ main.py
@@ -13,12 +13,14 @@ def iniciar_figura_nova(event):
     global figura_nova                         
 
     tipo = tipo_figura_var.get()
-
+    cor = cores[cor_var.get()]
+    cor_borda = cores[cor_borda_var.get()]
+    largura = espessuras[largura_borda_var.get()]
     if tipo == "Rabisco":
-        figura_nova = ("rabisco",[(event.x,event.y,)], cores[cor_var.get()]) #rabisco é o único que precisa de uma lista de pontos, por isso o caso especial.
+        figura_nova = ("rabisco",[(event.x,event.y,)], cor, cor_borda, largura) #rabisco é o único que precisa de uma lista de pontos, por isso o caso especial.
 
     else:
-        figura_nova = (tipos[tipo], (event.x,event.y,event.x,event.y), cores[cor_var.get()])
+        figura_nova = (tipos[tipo], (event.x,event.y,event.x,event.y), cor, cor_borda, largura)
 
 # Quando mouse é movido com o botão pressionado
 def atualizar_figura_nova(event):
@@ -27,8 +29,12 @@ def atualizar_figura_nova(event):
         figura_nova[1].append((event.x, event.y))
 
     else : 
-        tipo = figura_nova[0] #n fica mais fixo em linha, depende da figura dada na tupla agora.
-        figura_nova = (tipo, (figura_nova[1][0], figura_nova[1][1], event.x, event.y), cores[cor_var.get()])
+        tipo = figura_nova[0]  #n fica mais fixo em linha, depende da figura dada na tupla agora.
+        cor = figura_nova[2]
+        cor_borda = figura_nova[3]
+        largura = figura_nova[4]
+        figura_nova = (tipo, (figura_nova[1][0], figura_nova[1][1], event.x, event.y), cor, cor_borda, largura)
+
     desenhar_figuras()
     desenhar_figura_nova()
 
@@ -38,7 +44,7 @@ def incluir_figura_nova(event):
         figuras.append(figura_nova) 
     desenhar_figuras()
 
-def desenhar_figuras():
+def desenhar_figuras(): # generaliza os casos com lógica repetida
     desenhos = {
         "linha": canvas.create_line,
         "retangulo": canvas.create_rectangle,
@@ -47,23 +53,20 @@ def desenhar_figuras():
 
     canvas.delete("all")
 
-    for fig, values, cor in figuras:
+    for fig, values, cor, cor_borda, largura in figuras:
 
         if fig == "rabisco":
-            canvas.create_line(values, fill=cor)
+            canvas.create_line(*values, fill = cor, width = largura) #rabisco não pode ficar "sem preenchimento", igual à linha
+        
+        elif fig == "linha":
+            canvas.create_line(*values, fill = cor, width = largura) #linha não pode ficar "sem preenchimento", igual à rabisco
 
         elif fig == "circulo":
             novo_values = desenhar_circulo(values)
-            canvas.create_oval(
-                *novo_values,
-                outline=cor
-            )
+            canvas.create_oval(*novo_values, fill=cor, outline=cor_borda, width=largura)
 
         else:
-            desenhos[fig](
-                *values,
-                outline=cor
-            )
+            desenhos[fig](*values,fill=cor,outline=cor_borda,width=largura)
 
 def desenhar_figura_nova(): #mesma lógica dos dicts anteriores, organização e generalização.
     desenhos = {
@@ -72,20 +75,20 @@ def desenhar_figura_nova(): #mesma lógica dos dicts anteriores, organização e
         "oval": canvas.create_oval
     }
     
-    fig, values, cor = figura_nova
+    fig, values, cor, cor_borda, largura = figura_nova
      
     if fig == "rabisco":
-        canvas.create_line(values, dash = (4,2), fill = cor)
+        canvas.create_line(values, dash = (4,2), fill = cor,width=largura)
+    
+    elif fig == "linha":
+        canvas.create_line(values, dash = (4,2), fill = cor,width=largura)
 
     elif fig == "circulo":
         novo_values = desenhar_circulo(values)
-        canvas.create_oval(novo_values[0],novo_values[1],novo_values[2],novo_values[3], dash = (4,2), outline = cor)
-
-    elif fig == "oval":
-        canvas.create_oval(values[0],values[1],values[2],values[3], dash = (4,2), outline = cor)
+        canvas.create_oval(*novo_values, dash = (4,2), fill = cor)
 
     else:
-        desenhos[fig](values[0],values[1],values[2],values[3], dash = (4,2), outline = cor)
+        desenhos[fig](*values, dash = (4,2), fill=cor)
 
       
     
@@ -114,7 +117,7 @@ def desenhar_circulo(values):  #caso especial do oval
 
 def incompleta(figura):  #generalizei novamente, apenas deixando rabisco como caso especial.
     
-    fig, values, _ = figura
+    fig, values, *_ = figura
     if fig == "rabisco":
         return len(values) <= 1
     
@@ -123,37 +126,8 @@ def incompleta(figura):  #generalizei novamente, apenas deixando rabisco como ca
     
 
 
-def iniciar_figura_nova(event):
-
-    tipos = {
-        "Linha": "linha",
-        "Retângulo": "retangulo",
-        "Oval": "oval",
-        "Círculo": "circulo"
-    }
-
-    global figura_nova
-
-    tipo = tipo_figura_var.get()
-    cor = cores[cor_var.get()]
-
-    if tipo == "Rabisco":
-        figura_nova = ("rabisco", [(event.x, event.y)], cor)
-    else:
-        figura_nova = (
-            tipos[tipo],
-            (event.x, event.y, event.x, event.y),
-            cor
-        )
-
-
-
-
-
-
-
-
-#******* MAIN *******#
+              
+#/*/*/*/*/*/*/ MAIN /*/*/*/*/*/*/
 
 figuras = []       # Todas as figuras desenhadas
 figura_nova = None # Figura que está sendo desenhada, mas ainda não foi incluída em figuras
@@ -161,8 +135,9 @@ figura_nova = None # Figura que está sendo desenhada, mas ainda não foi inclu�
 root = Tk()
 frame = Frame(root)
 
-
+#dicionarios de cores e espessuras para os option menus
 cores = {
+    "Sem preenchimento": None,
     "Preto": "black",
     "Vermelho": "red",
     "Azul": "blue",
@@ -171,43 +146,65 @@ cores = {
     "Laranja": "orange",
     "Roxo": "purple"
 }
+espessuras = {
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "5": 5,
+    "8": 8
+}
+
 
 
 # Widgets arranjados com Layout grid dentro de frame
 paddings = {'padx': 5, 'pady': 5} 
 
+
+frame_controles = ttk.Frame(frame)#criei outro frame pra separar os textos do canvas, mas não é necessário, só pra organização visual mesmo.
+frame_controles.grid(column=0, row=0, sticky=N)
+
+#option menu & Label das espessuras e das cores das espessuras
+cor_borda_var = StringVar(root)
+cor_borda_var.set("Preto")
+
+largura_borda_var = StringVar(root)
+largura_borda_var.set("2")
+
+#cor da borda
+label_cor_borda = ttk.Label(frame_controles, text="Cor da borda:")
+label_cor_borda.grid(column=0, row=4, sticky=W, **paddings)
+option_menu_cor_borda = ttk.OptionMenu( frame_controles,cor_borda_var, cor_borda_var.get(), *cores.keys())
+option_menu_cor_borda.grid(column=0, row=5, sticky=W, **paddings)
+
+#borda
+label_largura_borda = ttk.Label(frame_controles, text="Grossura da borda:")
+label_largura_borda.grid(column=0, row=6, sticky=W, **paddings)
+option_menu_largura = ttk.OptionMenu(frame_controles,largura_borda_var, largura_borda_var.get(),*espessuras.keys())
+option_menu_largura.grid(column=0, row=7, sticky=W, **paddings)
+
+
+# option menu & Label das cores
 cor_var = StringVar(root)
-cor_var.set("Preto")
+cor_var.set("Sem preenchimento")
+
+option_menu_cor = ttk.OptionMenu( frame_controles,cor_var, cor_var.get(), *cores.keys())
+option_menu_cor.grid(column=0, row=3, sticky=W, **paddings)
+
+label_cor = ttk.Label(frame_controles, text = "Cor de preenchimento:")
+label_cor.grid(column = 0, row=2, sticky = W, **paddings)
 
 
-option_menu_cor = ttk.OptionMenu(
-    frame,
-    cor_var,
-    "Preto",
-    *cores.keys()
-)
-
-option_menu_cor.grid(column=3, row=0, sticky=W, **paddings)
-
-
-
-
-cor = cores[cor_var.get()]
-
-
-# label
-label = ttk.Label(frame,  text='Tipo da figura:')
-label.grid(column=0, row=0, sticky=W, **paddings)
-
-# option menu
+# option menu  & Label das figuras
 tipo_figura_var = StringVar(root) # Guarda o tipo de figura selecionado no option menu (linha ou rabisco)
-option_menu = ttk.OptionMenu(frame, tipo_figura_var,
+option_menu = ttk.OptionMenu(frame_controles, tipo_figura_var,
                             'Linha', 'Linha', 'Rabisco','Retângulo','Oval','Círculo')
-option_menu.grid(column=1, row=0, sticky=W, **paddings)
+option_menu.grid( column=0, row=1, sticky=W, **paddings)
+label = ttk.Label(frame_controles,  text='Tipo da figura:')
+label.grid(column=0, row=0, sticky=W, **paddings)
 
 # Área de desenho
 canvas = Canvas(frame, bg='white', width=600, height=600)
-canvas.grid(column=0, row=1, columnspan=2, sticky=W, **paddings)
+canvas.grid(column=1, row=0, sticky=NSEW, **paddings)
 
 frame.pack()
 
